134a8571bfc1a1bf259bc4fa06bde6b5531e2104
Indra Bagus <indra.bagus@gmail.com>
2017-10-04 16:30:37 +0700

    first time using $http

--- sysinfo_website/static/js/sysinfo.js
+++ sysinfo_website/static/js/sysinfo.js
@@ -1,35 +1,17 @@
-var app = angular.module('sysinfoApp',['angular-sysinfo']);
-
-app.controller('sysinfoCtrl',function($scope,$log,streamStatus,smartlockService,Constants){
-    $scope.constants = Constants;
-    $scope.lockstatus = streamStatus;
-    smartlockService.getAccount()
-    .then(
-        function(retvalue){
-            $scope.lockstatus.collection = retvalue.results;
-            app.globcoll = retvalue.results;
-        },
-        function(error){
-            $log.error(error);
-        }
-    );
-    
-});
-
-angular.module('ui.bootstrap.demo', ['ngAnimate', 'ngSanitize', 'ui.bootstrap']);
-angular.module('ui.bootstrap.demo').controller('TabsDemoCtrl', function ($scope, $window) {
-  $scope.tabs = [
-    { title:'Dynamic Title 1', content:'Dynamic content 1' },
-    { title:'Dynamic Title 2', content:'Dynamic content 2', disabled: true }
-  ];
-
-  $scope.alertMe = function() {
-    setTimeout(function() {
-      $window.alert('You\'ve selected the alert tab!');
-    });
-  };
-
+var app = angular.module('sysinfoApp', ['ngAnimate', 'ngSanitize', 'ui.bootstrap']);
+app.controller('linuxsysinfoCtrl', function ($scope,$http) {
   $scope.model = {
-    name: 'Tabs'
+    "System": "xLinux",
+    "Linux Distribution" :"LinuxMint - 18.2 - sonya",
+    "Architecture":"64bit ELF"    
   };
+  $http.get('/api/v1/linuxsysinfo.json',{
+    params:{}
+  }).then(function (response) {
+    console.log(response.data)
+    $scope.linuxsysinfo = response.data    
+  },function (error) {
+    console.log(error.data)
+  });
+
 });
